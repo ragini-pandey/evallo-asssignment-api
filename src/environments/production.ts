999ae93709c1a76cc84cc4a9991ae6fbe5e546ed
Ragini Pandey <99394366+ragini-pandey@users.noreply.github.com>
2024-03-18 23:45:36 +0530

    Update production.ts

--- src/environments/production.ts
+++ src/environments/production.ts
@@ -1,5 +1,5 @@
 export const ENVIRONMENT = {
   ENVIRONMENT_NAME: 'production',
   PORT_VALUE: 8000,
-  MONGODB_URL: 'mongodb+srv://ragini-pandey:ragini-pandey@cluster0.sequaqn.mongodb.net/',
+  MONGODB_URL: 'MONGODB_URL',
 };
